169bc6396815ec62d653e0588bf2425d5aef92cb
Brett Johnson <brettzkyjo10@gmail.com>
2026-04-20 02:10:08 -0500

    Scroll

--- rust/src/webview_control.rs
+++ rust/src/webview_control.rs
@@ -3,7 +3,7 @@ use std::{cell::RefCell, rc::Rc};
 use dpi::PhysicalSize;
 use euclid::{Box2D, Point2D};
 use godot::{classes::{Control, Engine, IControl, Image, ImageTexture, InputEvent, InputEventMouse, InputEventMouseButton, InputEventMouseMotion, image::Format}, global, prelude::*};
-use servo::{MouseButtonEvent, MouseMoveEvent, RenderingContext, SoftwareRenderingContext, WebView, WebViewBuilder, WebViewDelegate, WebViewPoint};
+use servo::{MouseButtonEvent, MouseMoveEvent, RenderingContext, SoftwareRenderingContext, WebView, WebViewBuilder, WebViewDelegate, WebViewPoint, WheelDelta, WheelEvent, WheelMode};
 use url::Url;
 
 use crate::servo_manager::ServoManager;
@@ -73,24 +73,52 @@ impl IControl for WebViewControl {
         if let Ok(mouse_event) = event.clone().try_cast::<InputEventMouse>() {
             let position = mouse_event.get_position();
             if let Ok(button_event) = mouse_event.clone().try_cast::<InputEventMouseButton>() {
-                webview_event = Some(servo::InputEvent::MouseButton(
-                    MouseButtonEvent {
-                        action: match button_event.is_pressed() {
-                            true => servo::MouseButtonAction::Down,
-                            false => servo::MouseButtonAction::Up
-                        },
-                        button: match button_event.get_button_index() {
-                            global::MouseButton::LEFT => servo::MouseButton::Left,
-                            global::MouseButton::MIDDLE => servo::MouseButton::Middle,
-                            global::MouseButton::RIGHT => servo::MouseButton::Right,
-                            global::MouseButton::XBUTTON1 => servo::MouseButton::Back,
-                            global::MouseButton::XBUTTON2 => servo::MouseButton::Forward,
-                            _ => servo::MouseButton::Other(0 as u16)
-                        },
-                        point: WebViewPoint::Device(
-                            Point2D::new(position.x, position.y))
+                match button_event.get_button_index() {
+                    global::MouseButton::WHEEL_UP |
+                    global::MouseButton::WHEEL_DOWN |
+                    global::MouseButton::WHEEL_LEFT |
+                    global::MouseButton::WHEEL_RIGHT => {
+                        let factor = button_event.get_factor() as f64 * 16.0;
+                        webview_event = Some(servo::InputEvent::Wheel(WheelEvent {
+                            delta: WheelDelta {
+                                x: factor * match button_event.get_button_index() {
+                                    global::MouseButton::WHEEL_LEFT => 1.0,
+                                    global::MouseButton::WHEEL_RIGHT => -1.0,
+                                    _ => 0.0
+                                },
+                                y: factor * match button_event.get_button_index() {
+                                    global::MouseButton::WHEEL_UP => 1.0,
+                                    global::MouseButton::WHEEL_DOWN => -1.0,
+                                    _ => 0.0
+                                },
+                                z: 0.0,
+                                mode: WheelMode::DeltaPixel
+                            },
+                            point: WebViewPoint::Device(
+                                    Point2D::new(position.x, position.y))
+                        }))
+                    },
+                    _ => {
+                        webview_event = Some(servo::InputEvent::MouseButton(
+                            MouseButtonEvent {
+                                action: match button_event.is_pressed() {
+                                    true => servo::MouseButtonAction::Down,
+                                    false => servo::MouseButtonAction::Up
+                                },
+                                button: match button_event.get_button_index() {
+                                    global::MouseButton::LEFT => servo::MouseButton::Left,
+                                    global::MouseButton::MIDDLE => servo::MouseButton::Middle,
+                                    global::MouseButton::RIGHT => servo::MouseButton::Right,
+                                    global::MouseButton::XBUTTON1 => servo::MouseButton::Back,
+                                    global::MouseButton::XBUTTON2 => servo::MouseButton::Forward,
+                                    _ => servo::MouseButton::Other(0 as u16)
+                                },
+                                point: WebViewPoint::Device(
+                                    Point2D::new(position.x, position.y))
+                            }
+                        ));
                     }
-                ));
+                }
             } else if let Ok(_) = mouse_event.try_cast::<InputEventMouseMotion>() {
                 webview_event = Some(servo::InputEvent::MouseMove(MouseMoveEvent {
                     point:WebViewPoint::Device(Point2D::new(position.x,position.y)),
