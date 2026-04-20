3472cb2289a00e776c35df43e47ada7f0bf6d81d
Brett Johnson <brettzkyjo10@gmail.com>
2026-04-19 21:09:02 -0500

    Control displays webview 😄

--- rust/src/webview_control.rs
+++ rust/src/webview_control.rs
@@ -1,6 +1,7 @@
 use std::{cell::RefCell, rc::Rc};
 
-use godot::{classes::{Control, Engine, IControl}, prelude::*};
+use euclid::{Box2D, Point2D};
+use godot::{classes::{Control, Engine, IControl, Image, ImageTexture, image::Format}, prelude::*};
 use servo::{WebView, WebViewBuilder, WebViewDelegate};
 use url::Url;
 
@@ -12,49 +13,85 @@ enum ProxyEvent {
 }
 
 #[derive(GodotClass)]
-#[class(base=Control, init, tool, rename=WebView)]
+#[class(base=Control, tool, rename=WebView)]
 struct WebViewControl {
     base: Base<Control>,
-    webview: Option<WebView>,
+    webview: WebView,
     event_queue: Rc<RefCell<Vec<ProxyEvent>>>,
+    image_texture: Option<Gd<ImageTexture>>,
 }
 
 #[godot_api]
 impl IControl for WebViewControl {
-    fn ready(&mut self) {
+    fn init(base: Base<Control>) -> Self {
         let servo_manager = Engine::singleton()
             .get_singleton("ServoManager").expect("Failed to get singleton")
             .cast::<ServoManager>();
+        let event_queue = Rc::new(RefCell::new(Vec::new()));
         let webview =
             WebViewBuilder::new(
                     servo_manager.bind().get_servo(),
                     servo_manager.bind().get_window().get_rendering_context()
                 )
                 .delegate(Rc::new(Proxy {
-                    event_queue: self.event_queue.clone(),
+                    event_queue: event_queue.clone(),
                 }))
                 .url(Url::parse("https://google.com").expect("Failed to parse url"))
                 .build();
-        self.webview = Some(webview);
-        godot_print!("Ready");
+
+        Self {
+            base,
+            webview,
+            event_queue,
+            image_texture: None
+        }
+    }
+
+    fn draw(&mut self) {
+        if let Some(image_texture) = self.image_texture.clone() {
+            self.base_mut().draw_texture(&image_texture, Vector2::ZERO);
+        }
     }
 
     fn process(&mut self, _delta: f64) {
-        Engine::singleton()
+        let mut servo_manager = Engine::singleton()
             .get_singleton("ServoManager")
-            .expect("Failed to get singleton").cast::<ServoManager>()
-            .bind_mut()
-            .wake_if_needed();
+            .expect("Failed to get singleton").cast::<ServoManager>();
+        
+        {
+            servo_manager.bind_mut().wake_if_needed();
+        }
+            
 
         let events: Vec<ProxyEvent> = self.event_queue.borrow_mut().drain(..).collect();
         for event in events {
             match event {
                 ProxyEvent::UrlChanged(url) => {
-                    godot_print!("WebViewControl: URL changed to {url}");
+                    godot_print!("WebViewControl: URL changed to {}", url.as_str());
 
                 },
                 ProxyEvent::NewFrameReady => {
-                    godot_print!("WebViewControl: New frame ready");
+                    self.webview.paint();
+                    let servo_manager = servo_manager.bind();
+                    let window = servo_manager.get_window();
+                    let window_size = window.get_rendering_context().size();
+                    let image_option = window.get_rendering_context()
+                        .read_to_image(Box2D::new(Point2D::origin(), Point2D::new(window_size.width as i32, window_size.height as i32)));
+                    if let Some(image_buffer) = image_option {
+                        let data = PackedByteArray::from(image_buffer.as_raw().as_slice());
+                        let image = Image::create_from_data(
+                            window_size.width as i32, window_size.height as i32,
+                            false, Format::RGBA8, &data);
+                        if let Some(mut image_texture) = self.image_texture.clone() {
+                            image_texture.set_image(image.as_ref());
+                        } else {
+                            let image_texture = ImageTexture::create_from_image(image.as_ref());
+                            self.image_texture = image_texture;
+                        }
+                        if image.is_some() {
+                            self.base_mut().queue_redraw();
+                        }
+                    }
                 }
             }
         }
@@ -67,12 +104,10 @@ struct Proxy {
 
 impl WebViewDelegate for Proxy {
     fn notify_url_changed(&self, _webview: WebView, url: Url) {
-        godot_print!("Proxy: URL changed");
         self.event_queue.borrow_mut().push(ProxyEvent::UrlChanged(url));
     }
 
     fn notify_new_frame_ready(&self, _webview: WebView) {
-        godot_print!("Proxy: New frame ready");
         self.event_queue.borrow_mut().push(ProxyEvent::NewFrameReady);
     }
 }
