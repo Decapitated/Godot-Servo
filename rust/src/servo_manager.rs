c5facce67322ea44f322cc40119b245dc328182e
Brett Johnson <brettzkyjo10@gmail.com>
2026-04-24 14:41:50 -0500

    Enable devtools. Doesn't work though.

--- rust/src/servo_manager.rs
+++ rust/src/servo_manager.rs
@@ -23,6 +23,8 @@ impl IObject for ServoManager {
         let opts = Opts::default();
 
         let mut preferences = Preferences::default();
+        preferences.devtools_server_enabled = true;
+        preferences.devtools_server_listen_address = "127.0.0.1:6080".to_owned();
         preferences.user_agent = 
             "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:150.0) Gecko/20100101 Firefox/150.0".to_owned();
         
